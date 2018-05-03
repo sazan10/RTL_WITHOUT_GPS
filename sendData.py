59e0cf5600c06f13e9432cee985e8ff15b7f82c3
sazan10 <070bex432@ioe.edu.np>
2018-05-03 10:39:34 +0545

    eta

--- sendData.py
+++ sendData.py
@@ -40,7 +40,8 @@ def degreesToRadians(degrees): #function to convert degrees to radians, required
     return degrees * math.pi / 180;
 
 def distanceInmBetweenEarthCoordinates(lat1, lon1, lat2, lon2): #function to calculate distance between two gps coordinates
-    earthRadiusm = 6378137;#radius of the earth in meters
+    #earthRadiusm = 6378137;#radius of the earth in meters
+    earthRadiusm=6371000
     dLat = degreesToRadians(lat2-lat1)
     dLon = degreesToRadians(lon2-lon1)
     latt1 = degreesToRadians(lat1)
@@ -86,16 +87,17 @@ def save_mission():
     #Add commands
     inc=1
     for cmd in missionlist:
-        waypoint[inc]= {
+        if cmd.command!=22:
+            waypoint[inc]= {
             'lat': cmd.x,
             'lon': cmd.y,
             'alt': cmd.z,
             'command': cmd.command
-        }
-        inc=inc+1
+            }
+            inc=inc+1
     #print(waypoint)
     #p = requests.get("http://127.0.0.1:3000/waypoints", json=waypoint)
-    p = requests.get("https://nicwebpage.herokuapp.com/waypoints",json=json.dumps(waypoint))
+    #p = requests.get("https://nicwebpage.herokuapp.com/waypoints",json=json.dumps(waypoint))
 
 
 dist=[]
@@ -103,7 +105,8 @@ def calculate_dist():
     total_dist=0
     for i in range(len(waypoint)-1):
         try:
-            dist.append(distanceInmBetweenEarthCoordinates(waypoint[i+1]['lat'],waypoint[i+1]['lon'],waypoint[i+2]['lat'],waypoint[i+2]['lon']))
+            dist.append(distanceInmBetweenEarthCoordinates(waypoint[i]['lat'],waypoint[i]['lon'],waypoint[i+1]['lat'],waypoint[i+1]['lon']))
+            #print(i,dist[i])
             total_dist += dist[i]
         except Exception as e:
             #print(e)
@@ -113,10 +116,14 @@ def calculate_dist():
 checker=False
 check = True
 total=0
+divisor=1
+last_vel=0
 def send_data(threadName, delay):
     global checker
     global check
     global total
+    global divisor
+    global last_vel
     while 1:
         loc = vehicle.location.global_frame
         vel = vehicle.velocity
@@ -157,31 +164,40 @@ def send_data(threadName, delay):
             if vehicle.armed and check:
                 time1 = datetime.datetime.now()
                 check=False
-            if not check:
+            if not check :
                 try:
                     time2 = datetime.datetime.now()
                     flight_time=float((time2-time1).total_seconds())
-                    vel=float(vehicle.groundspeed)
+                    vel=float((last_vel+vehicle.groundspeed)/divisor)
+
                     #print("flight time",flight_time)
                     #print("total distance",total)
-                    est=float((float(total)-flight_time*vel)/vel+10+len(waypoint))
-                    if est<=0:
+                    est=float((float(total)-flight_time*vel)/3.5)
+                    #if est<=1 and (total-flight_time*vel)<=2:
+                        #total=0
+                    if est<=0.5:
                         est=0
                     data["est"]=est
-                    #print("estimated time:", est)
+                    #print(est)
+                    #print("vel:",vel)
+                    print("estimated time:", est, total, flight_time*vel)
+                    last_vel+=vehicle.groundspeed
+                    divisor+=1
                 except Exception as e:
                     #print(type(e))
                     #print(e.args)
                     pass
+
         #r = requests.get("http://127.0.0.1:3000/data",params=data)
-        r = requests.get("https://nicwebpage.herokuapp.com/data",params=data)
+        #r = requests.get("https://nicwebpage.herokuapp.com/data",params=data)
         #r = requests.get("http://photooverlay.com/nic/get_data.php",params=data)
 
-        if r.text == '1':
-        #if not checker:
+        #if r.text == '1':
+        if not checker:
             save_mission()
             checker=True
             total=calculate_dist()
+            print("total:",total)
 
 
 
